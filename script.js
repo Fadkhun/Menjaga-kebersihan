d1ab1b1fe322f245917b14469c82a99514e52789
Fadkhun <Fadhlyramadhana@gmail.com>
2025-09-21 14:27:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+highlight menu saat scroll window.addEventListener('scroll', () => { const sections = document.querySelectorAll('main section'); const tocLinks = document.querySelectorAll('.toc a'); let current = ''; sections.forEach(section => { const sectionTop = section.offsetTop; if (pageYOffset >= sectionTop - 60) { current = section.getAttribute('id'); } }); tocLinks.forEach(link => { link.classList.remove('active'); if (link.getAttribute('href') === #${current}) { link.classList.add('active'); } }); });
+
